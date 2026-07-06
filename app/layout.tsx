1abd792c44f8c9eb454cd8658743ea80eb734f7a
reece-create <reece@founditmarketing.com>
2026-07-06 13:11:38 -0500

    chore: inject Ads tag for Little Cakes

--- app/layout.tsx
+++ app/layout.tsx
@@ -62,6 +62,11 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} ${bricolage.variable}`}
     >
       <body className="min-h-[100dvh] antialiased">
+      {/* Ads Body Tag */}
+      <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MDKSLQ98"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
         <a
           href="#main-content"
           className="sr-only focus:not-sr-only focus:fixed focus:left-4 focus:top-4 focus:z-[100] focus:rounded-full focus:bg-teal focus:px-5 focus:py-2.5 focus:text-sm focus:font-semibold focus:text-teal-ink"
@@ -89,6 +94,12 @@ export default function RootLayout({
         {/* Reserve space so the fixed mobile order bar never covers the footer */}
         <div aria-hidden="true" className="h-[88px] lg:hidden" />
         <MobileOrderBar />
+      {/* Ads Tag */}
+      <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MDKSLQ98');` }} />
       </body>
     </html>
   );
